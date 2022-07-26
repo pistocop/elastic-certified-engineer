63a0e16b7ddf0bd14dadedfd312a3fc12c8df057
pistocop <simone.guardati42@gmail.com>
2022-07-26 17:44:39 +0200

    add cluster with custom generated certificates injection

--- dockerfiles/17_custom-cluster8x/notes.md
+++ dockerfiles/17_custom-cluster8x/notes.md
@@ -1,2 +1,22 @@
 # 17_custom-cluster8x
 > Hacking around to learn how certificate and security work on elasticsearch 8.x
+
+
+## Takeaways
+- Elasticsearch password must be > 8 characters
+- We can create the certificates externally and the inject them
+  - The certificate creation could be done using a yml file as input
+  - The certificates should be copied on specific folders reported on ENV variables, different per pod
+  - [!] The problem is "be sure" on how the permissions and owners are set for those files
+- The `elastic` password is set using the environment variables
+- How create the certificates? follow the steps under "setup" phase of the [official docker-compose](https://www.elastic.co/guide/en/elasticsearch/reference/current/docker.html#_prepare_the_environment)
+
+## Problem
+- [ ] currently the cluster start with problems on the elastic password,
+    - the certificates are correctly set because if we reset the password, the system correctly work
+    - Warning: looks like we have bad permission configurated
+      - ```
+        elasticsearch@5fbc7b5c3754:~$ bin/elasticsearch-reset-password -u elastic
+        WARNING: Owner of file [/usr/share/elasticsearch/config/users] used to be [root], but now is [elasticsearch]
+        WARNING: Owner of file [/usr/share/elasticsearch/config/users_roles] used to be [root], but now is [elasticsearch]
+        ```
---
+++ dockerfiles/17_custom-cluster8x/setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -x
+
+# es01
+docker exec -u elasticsearch 17_custom-cluster8x_es01_1 mkdir -p /usr/share/elasticsearch/config/certs/ca/
+docker exec -u elasticsearch 17_custom-cluster8x_es01_1 mkdir -p /usr/share/elasticsearch/config/certs/es01/
+
+docker cp ./mycert/certs/ca/ca.crt 17_custom-cluster8x_es01_1:/usr/share/elasticsearch/config/certs/ca/
+docker cp ./mycert/certs/es01 17_custom-cluster8x_es01_1:/usr/share/elasticsearch/config/certs/
+
+docker exec -u root 17_custom-cluster8x_es02_1 chown -R root:root /usr/share/elasticsearch/config;
+docker exec -u root 17_custom-cluster8x_es01_1 chown -R root:elasticsearch /usr/share/elasticsearch/config/certs;
+docker exec -u root 17_custom-cluster8x_es01_1 find /usr/share/elasticsearch/config/certs/ -type d -exec chmod 750 \{\} \;;
+docker exec -u root 17_custom-cluster8x_es01_1 find /usr/share/elasticsearch/config/certs/ -type f -exec chmod 640 \{\} \;;
+
+
+# es02
+docker exec -u elasticsearch 17_custom-cluster8x_es02_1 mkdir -p /usr/share/elasticsearch/config/certs/ca/
+docker exec -u elasticsearch 17_custom-cluster8x_es02_1 mkdir -p /usr/share/elasticsearch/config/certs/es02/
+
+docker cp ./mycert/certs/ca/ca.crt 17_custom-cluster8x_es02_1:/usr/share/elasticsearch/config/certs/ca/
+docker cp ./mycert/certs/es02 17_custom-cluster8x_es02_1:/usr/share/elasticsearch/config/certs/
+
+docker exec -u root 17_custom-cluster8x_es02_1 chown -R root:root /usr/share/elasticsearch/config;
+docker exec -u root 17_custom-cluster8x_es02_1 chown -R root:elasticsearch /usr/share/elasticsearch/config/certs;
+docker exec -u root 17_custom-cluster8x_es02_1 find /usr/share/elasticsearch/config/certs/ -type d -exec chmod 750 \{\} \;;
+docker exec -u root 17_custom-cluster8x_es02_1 find /usr/share/elasticsearch/config/certs/ -type f -exec chmod 640 \{\} \;;
+
+
+# es03
+docker exec -u elasticsearch 17_custom-cluster8x_es03_1 mkdir -p /usr/share/elasticsearch/config/certs/ca/
+docker exec -u elasticsearch 17_custom-cluster8x_es03_1 mkdir -p /usr/share/elasticsearch/config/certs/es03/
+
+docker cp ./mycert/certs/ca/ca.crt 17_custom-cluster8x_es03_1:/usr/share/elasticsearch/config/certs/ca/
+docker cp ./mycert/certs/es03 17_custom-cluster8x_es03_1:/usr/share/elasticsearch/config/certs/
+
+docker exec -u root 17_custom-cluster8x_es03_1 chown -R root:root /usr/share/elasticsearch/config;
+docker exec -u root 17_custom-cluster8x_es03_1 chown -R root:elasticsearch /usr/share/elasticsearch/config/certs;
+docker exec -u root 17_custom-cluster8x_es03_1 find /usr/share/elasticsearch/config/certs/ -type d -exec chmod 750 \{\} \;;
+docker exec -u root 17_custom-cluster8x_es03_1 find /usr/share/elasticsearch/config/certs/ -type f -exec chmod 640 \{\} \;;
